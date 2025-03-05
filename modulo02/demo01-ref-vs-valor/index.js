ff2e8c7dcf5884c06154ccf134197dfa1171076d
matheusgondra <matheusgondra951@gmail.com>
2025-03-05 19:00:48 -0300

    tipo de valor e tipo de referencia

---
+++ modulo02/demo01-ref-vs-valor/index.js
@@ -0,0 +1,19 @@
+const { deepStrictEqual } = require("node:assert");
+
+let counter = 0;
+let counter2 = counter;
+counter2++;
+
+// tipo primitivo gera uma cópia em memória
+deepStrictEqual(counter, 0);
+deepStrictEqual(counter2, 1);
+
+const item = { counter: 0 };
+const item2 = item;
+
+// tipo de referência, copia o endereço de memória
+item2.counter++;
+deepStrictEqual(item, { counter: 1 });
+
+item.counter++;
+deepStrictEqual(item2, { counter: 2 });

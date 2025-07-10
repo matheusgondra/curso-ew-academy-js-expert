1a5f9281b354154a2e889361afa03edf379e23a9
matheusgondra <matheusgondra951@gmail.com>
2025-07-10 15:20:07 -0300

    inicio da aula 2 de streams

---
+++ modulo10/aula02/examples/1.concat-streams-pass-through.js
@@ -0,0 +1,31 @@
+import { Writable } from "stream"
+import axios from "axios";
+
+const API_01 = "http://localhost:3000";
+const API_02 = "http://localhost:4000";
+
+const requests = await Promise.all([
+    axios({
+        method: "get",
+        url: API_01,
+        responseType: "stream"
+    }),
+    axios({
+        method: "get",
+        url: API_02,
+        responseType: "stream"
+    })
+]);
+
+const result = requests.map(({ data }) => data);
+
+const output = Writable({
+    write(chunk, encoding, callback) {
+        const data = chunk.toString();
+        console.log(`data: ${data}`);
+        callback();
+    }
+});
+
+result[0].pipe(output);
+result[1].pipe(output);

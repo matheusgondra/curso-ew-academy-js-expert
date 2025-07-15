82b809297f119f3ecd9079345294b6a1ee4d3ea0
matheusgondra <matheusgondra951@gmail.com>
2025-07-15 09:16:41 -0300

    finalizando exemplo 1

--- modulo10/aula02/examples/1.concat-streams-pass-through.js
+++ modulo10/aula02/examples/1.concat-streams-pass-through.js
@@ -1,4 +1,4 @@
-import { Writable } from "stream"
+import { PassThrough, Writable } from "stream"
 import axios from "axios";
 
 const API_01 = "http://localhost:3000";
@@ -17,15 +17,32 @@ const requests = await Promise.all([
     })
 ]);
 
-const result = requests.map(({ data }) => data);
+const results = requests.map(({ data }) => data);
 
 const output = Writable({
     write(chunk, encoding, callback) {
-        const data = chunk.toString();
-        console.log(`data: ${data}`);
+        const data = chunk.toString().replace(/\n/, "");
+        // ?=- -> faz procurar a partir do - e olhar para trás
+        // (?<name>.*) -> procura pelo contaudo dentro das aspas apos o : e extrai somente o nome
+        const name = data.match(/:"(?<name>.*)(?=-)/).groups.name;
+        console.log(`[${name.toLowerCase()}] ${data}`);
         callback();
     }
 });
 
-result[0].pipe(output);
-result[1].pipe(output);
+function merge(streams) {
+    return streams.reduce((prev, current, index, items) => {
+        // impede que a stream feche sozinha
+        current.pipe(prev, { end: false });
+
+        // como colocamos end: false, vamos manipular manualmente quando o nosso current
+        // terminar. Quando ele terminar, vamos verificar se todos no pipeline se encerraram
+        // ele vai então forçar a cadeia do anterior a se fechar
+        current.on("end", () => items.every(s => s.ended) && prev.end());
+
+        return prev;
+    }, new PassThrough());
+}
+// result[0].pipe(output);
+// result[1].pipe(output);
+merge(results).pipe(output);
